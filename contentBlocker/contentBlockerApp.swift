a76c361a50da5beb6ca2f3d70a490c2084436448
ds <strides.glow-0t@icloud.com>
2023-04-13 15:23:39 +0200

    Initial Commit

---
+++ contentBlocker/contentBlockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  contentBlockerApp.swift
+//  contentBlocker
+//
+//  Created by Daniele Stelitano on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct contentBlockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
